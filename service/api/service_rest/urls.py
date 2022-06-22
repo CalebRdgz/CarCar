51ac12d37aa0f0e38b347ec9325d0eea17a5390c
Mark Esposito <mjesposito3@gmail.com>
2022-06-21 21:02:55 -0600

    adding progress on views

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path,
 from .views import api_service_list, api_service_details
 
 urlpatterns = [
-    path('service', api_service_list, name="api_service_list"),
+    path('service/', api_service_list, name="api_service_list"),
     path('service/<int:pk>', api_service_details, name="api_service_detail")
 ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,18 +1,64 @@
+from black import Mode
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
+
+from pkg_resources import require
 from common.json import ModelEncoder
 from .models import ServiceAppointment, Technician, AutomobileVO
 
 # Create your views here.
 
+class AutomobilesVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "id",
+        "color",
+        "year", 
+        "vin"
+    ]
+
+class ServiceAppointmentEncoder(ModelEncoder):
+    model = ServiceAppointment
+    properties = [
+    "customer name",
+    "reason",
+    "technician"
+    ]
+
+    encoders={
+    "automobile": AutomobileVO(),
+    }
+
+
+class TechnicainEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        'technician_name',
+        'employee_name',
+    ]
+
 
 @require_http_methods(["GET", "POST"])
 def api_service_list(request):
-    pass
+    if request.method == "GET":
+        appts = ServiceAppointment.objects.all()
+        return JsonResponse(
+            appts,
+            encoder=ServiceAppointmentEncoder,
+            safe = False
+        )
+    else: #POST
+        content = json.loads(request.body)
+        appt = ServiceAppointment.objects.create(**content)
+        return JsonResponse(
+            appt,
+            encoder=ServiceAppointmentEncoder,
+            safe = False
+        )
 
 
-
-def api_service_details(request):
-    pass
+@require_http_methods(["GET", "PUT", ""])
+def api_service_details(request, pk):
+    
