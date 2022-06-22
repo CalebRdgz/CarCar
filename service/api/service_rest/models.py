ca23ce9a4dfa96e4e330e8bbd23d4b60ef3079b8
Mark Esposito <mjesposito3@gmail.com>
2022-06-21 18:13:53 -0600

    models complete, views drafted, urls drafted

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AutomobileVO(models.Model):
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+
+
+class Technician(models.Model):
+    technician_name = models.CharField(max_length=50)
+    employee_number = models.SmallIntegerField(unique=True)
+
+
+class ServiceAppointment(models.Model):
+    customer_name = models.CharField(max_length=50)
+    reason = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name = "technician",
+        on_delete=models.PROTECT,
+    )
+
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="automobile",
+        on_delete=models.CASCADE,
+    )
+
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import ServiceAppointment, Technician, AutomobileVO
 
 # Create your views here.
+
+
+@require_http_methods(["GET", "POST"])
+def api_service_list(request):
+    pass
+
+
+
+def api_service_details(request):
+    pass
